804fd76572152943817489c26d4e40062c1da758
Brian Baltz <brian.a.baltz@intel.com>
2015-09-02 17:02:32 -0700

    fixing KW scan

--- KW/kwscan-firmware.sh
+++ KW/kwscan-firmware.sh
@@ -2,6 +2,7 @@
 export PATH=$PATH:~/kw/kw10.3.0/bin
 url='http://findlay-ubuntu.amr.corp.intel.com:8080'
 proj="firmware"
-kwinject make -C thunderdome/projects/atlaspeak_atlasedge setup image
+make -C thunderdome/projects/atlaspeak_atlasedge setup
+kwinject make -C thunderdome/projects/atlaspeak_atlasedge image
 kwbuildproject --url $url/$proj -f -o ~/kw/kwtables kwinject.out
 kwadmin --url $url load $proj ~/kw/kwtables/
