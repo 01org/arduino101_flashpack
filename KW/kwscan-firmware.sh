f6dad84a34798d02617ae5b92f90ef1f36679e6d
Brian Baltz <brian.a.baltz@intel.com>
2015-09-08 08:39:44 -0700

    Fix missing include errors

--- KW/kwscan-firmware.sh
+++ KW/kwscan-firmware.sh
@@ -4,5 +4,7 @@ url='http://findlay-ubuntu.amr.corp.intel.com:8080'
 proj="firmware"
 make -C thunderdome/projects/atlaspeak_atlasedge setup
 kwinject make -C thunderdome/projects/atlaspeak_atlasedge image
-kwbuildproject --url $url/$proj -f -o ~/kw/kwtables kwinject.out
+kwbuildproject --url $url/$proj -f -o ~/kw/kwtables \
+   --add-compiler-options "-I ${PWD}/thunderdome/thirdparty/gcc-arc-elf32/arc-elf32/include" \
+   kwinject.out
 kwadmin --url $url load $proj ~/kw/kwtables/
