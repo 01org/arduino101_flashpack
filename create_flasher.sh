fa2a3a0bbb6d9a5a485a7cc99ece67ab22fccac3
Brian Baltz <brian.a.baltz@intel.com>
2016-03-09 10:16:42 -0800

    Rework flashpack script for new location
    Signed-off-by: Brian Baltz <brian.a.baltz@intel.com>

--- create_flasher.sh
+++ create_flasher.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
+set -e
 topdir=$(dirname `readlink -e $0`)
 cd $topdir
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 build_directory tag"
-  exit 1
-fi
 # verify build directory exists
-dir=$(readlink -e ../$1)
-fwdir=$dir/out/current/firmware
+fwdir=$topdir/../out/current/firmware
 if [ ! -d $fwdir ]; then
   echo "$fwdir does not exist."
   exit 1
 fi
 # create readable tag for flasher package
-tag=$(echo $2 |sed -e "s/\//-/g" -)
+tag=$(echo $1 |sed -e "s/\//-/g" -)
 echo $tag
-flasher=../flash-${tag}.zip
-symlink=../flashpack.zip
+flasher=flashpack_${tag}.zip
 
 # copy .bin and partition.conf files into flasher package
-mkdir arduino101_flasher/images/
-rsync -avm --include='*.bin' --include='*partition.conf' -f 'hide,! */' $fwdir/ arduino101_flasher/images/firmware/
+rm -rf images
+mkdir images/
+rsync -avm --include='*.bin' --include='*partition.conf' -f 'hide,! */' $fwdir/ images/firmware/
 
 # create flasher package
-rsync -rav arduino101_flasher/ arduino101_flasher_${tag}
+cd ..
+rsync -rav --exclude='.git' --exclude='create_flasher.sh' arduino101_flashpack/ arduino101_flasher_${tag}
+rm -rf $flasher
 zip -r $flasher arduino101_flasher_${tag}/
-ln -s $(basename $flasher) $symlink
+ln -sf $(basename $flasher) flashpack.zip
